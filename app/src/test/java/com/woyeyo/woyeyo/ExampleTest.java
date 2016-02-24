f337531f7b5d002d653827b78b15e358511795d9
Cheng <chengdazhi971014@gmail.com>
2016-02-24 11:36:57 +0800

    commit 2016/2/24, add drawer/test/banner repos.

---
+++ app/src/test/java/com/woyeyo/woyeyo/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.woyeyo.woyeyo;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by chengdazhi on 2/24/16.
+ */
+public class ExampleTest extends InstrumentationTestCase {
+
+
+}
